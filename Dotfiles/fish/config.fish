c7f007212886a2f11ae5f56ade647db8f62c6476
MystrE <73987620+shadow64321@users.noreply.github.com>
2024-05-23 23:37:15 +1000

    Add files via upload

--- Dotfiles/fish/config.fish
+++ Dotfiles/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
+    zoxide init fish | source
     alias music='musikcube'
     alias fishy='asciiquarium'
     alias rfishy='asciiquarium | lolcat'
@@ -8,12 +9,14 @@ if status is-interactive
     alias ascii='ascii-image-converter'
     alias update='sudo nixos-rebuild switch'
     alias sysedit='sudo vim /etc/nixos/configuration.nix'
+    alias anime='bash ~/Documents/ani-cli/ani-cli'
+    alias drama='bash ~/Documents/dra-cla/dra-cla'
     alias sysview='bat /etc/nixos/configuration.nix'
     alias syspacks='bat /etc/current-system-packages'
     alias rename='mv'
     alias rebsp='bash ~/.config/bspwm/bspwmrc'
     alias binds='bat ~/.config/sxhkd/sxhkdrc'
     # neofetch
-    alias cya='echo "BYE" && shutdown now'
+    alias img='feh'
     cowsay "Life is like a weiner, sometimes it gets hard for no reason" | lolcat
 end
--- Dotfiles/nvim/lua/plugins/lsp-config.lua
+++ Dotfiles/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "clangd", "cmake", "gopls", "autotools_ls", "taplo", "lemminx", "hydra_lsp", "zls" }
+        ensure_installed = { "lua_ls", "clangd" }
       })
     end
   },
@@ -17,16 +17,16 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.clangd.setup({})
-      lspconfig.cmake.setup({})
-      lspconfig.gopls.setup({})
-      lspconfig.autotools_ls.setup({})
-      lspconfig.markdown_oxide.setup({})
-      lspconfig.taplo.setup({})
-      lspconfig.lemminx.setup({})
-      lspconfig.hydra_lsp.setup({})
-      lspconfig.zls.setup({})
+      lspconfig.lua_ls.setup{}
+      lspconfig.clangd.setup{}
+      -- lspconfig.cmake.setup({})
+      -- lspconfig.gopls.setup({})
+      -- lspconfig.autotools_ls.setup({})
+      -- lspconfig.markdown_oxide.setup({})
+      -- lspconfig.taplo.setup({})
+      -- lspconfig.lemminx.setup({})
+      -- lspconfig.hydra_lsp.setup({})
+      -- lspconfig.zls.setup({})
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
